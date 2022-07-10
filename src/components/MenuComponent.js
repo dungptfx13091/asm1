eb3a7ace5a5b65c0611577a88bd0d2dd57de57f9
dungptfx13091 <dungptfx13091@funix.edu.vn>
2022-07-10 16:38:59 +0900

    MenuComponent Created

---
+++ src/components/MenuComponent.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { Media } from "";
+
+class Menu extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row"></div>
+      </div>
+    );
+  }
+}
+
+export default Menu;
